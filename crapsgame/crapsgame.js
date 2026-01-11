e1c1241d2644f8649a34d11ac9f2c1cf12ea438b
Eleanya Onuma <onumaeleanyagift@gmail.com>
2026-01-11 05:44:52 +0100

    Scaled dice animation upon rolling

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -105,17 +105,27 @@ function setBetAmount(betAmount) {
 }
 
 function rollDice() {
+  formatDiceScale();
   document.getElementById(crapsRollDiceButton).style.display = "none";
   const diceRollElement = document.getElementById(
     crapsRollDiceAnimationContainer
   );
-  
+
   rollADie({
     element: diceRollElement,
     numberOfDice: 2,
     callback: processDiceResult,
     delay: 1000000,
   });
+}
+
+function formatDiceScale() {
+  const vw = window.innerWidth = 80
+  const vh = window.innerHeight = 0.8
+  const widthScale = Math.min(700, vw, vh)
+  const heightScale = widthScale * 0.714
+  const scale = heightScale / 494.6592
+  document.getElementById(crapsRollDiceAnimationContainer).style.transform = "scale(" + scale + ")";
 }
 
 function processDiceResult(diceResult) {
