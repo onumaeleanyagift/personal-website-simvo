19c293f5b36df18fce9e11e56749d4b8463b16ee
Eleanya Onuma <onumaeleanyagift@gmail.com>
2026-01-26 09:40:48 +0100

    Nested loops exercise in python

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 
 for i in range(1, 51):
+    # Precondition: not a multiple of 4
+    if i % 4 == 0:
+        continue
+
     if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
     if i % 3 == 0:
@@ -8,3 +12,15 @@ for i in range(1, 51):
         print("Buzz")
     else:
         print(i)
+
+
+# Nested Loops Challenge
+rows = 9
+
+print("For rows = " + str(rows))
+
+for row in range(1, rows + 1):
+    printVal = ""
+    for column in range(row):
+        printVal += "*"
+    print(printVal)
