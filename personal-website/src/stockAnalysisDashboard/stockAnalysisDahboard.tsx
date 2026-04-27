e25907e4f67c4ad7c33aab6151ed107b056adbde
Eleanya Onuma <onumaeleanyagift@gmail.com>
2026-04-27 01:30:06 +0100

    Implemented conditional rendering on stock data response

--- personal-website/src/stockAnalysisDashboard/stockAnalysisDahboard.tsx
+++ personal-website/src/stockAnalysisDashboard/stockAnalysisDahboard.tsx
@@ -1,34 +1,74 @@
 import { useState } from "react";
 import { analyzeStock } from "./stockAnalysisDahboard";
+import { Oval } from "react-loader-spinner";
 
 function StockAnalysisDashboard() {
-    const [stockData, setStockData] = useState('123');
-    const [stockSymbol, setStockSymbol] = useState('AAAA')
+  const [stockData, setStockData] = useState("123");
+  const [stockSymbol, setStockSymbol] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [gotData, setGotData] = useState(false);
 
-    async function runStockAnalysis() {
-      const gotStockData = await analyzeStock(stockSymbol);
-      setStockData(gotStockData)
+  function goBack() {
+    setGotData(false);
+    setIsLoading(false);
   }
 
+  async function runStockAnalysis() {
+    setIsLoading(true);
+    const gotStockData = await analyzeStock(stockSymbol);
+    if (gotStockData) {
+      setStockData(gotStockData);
+      setGotData(true);
+      setIsLoading(false);
+    } else {
+      goBack();
+    }
+  }
+
+  if (gotData) {
+    return (
+      <div>
+        <div onClick={() => goBack()}>Back</div>
+        <div>{JSON.stringify(stockData)}</div>
+      </div>
+    );
+  }
 
   return (
     <>
       <div>
         <div id="stockAnalysisDashboardTitle">STOCK ANALYSIS DASHBOARD</div>
-        <div id="stockAnalysisDashboardSubtitle">
-          Put in a stock symbol you'd like to analyze (e.g. MSFT)
-        </div>
-        <input
-          value={stockSymbol}
-          onChange={(e) => setStockSymbol(e.target.value)}
-        ></input>
-        <button
-          id="stockAnalysisDashboardButton"
-          onClick={() => runStockAnalysis()}
-        >
-          Analyze
-        </button>
-        <div>{JSON.stringify(stockData)}</div>
+        {isLoading ? (
+          <div>
+            <Oval
+              height={80}
+              width={80}
+              color="#4fa94d"
+              visible={true}
+              ariaLabel="oval-loading"
+              secondaryColor="#4fa94d"
+              strokeWidth={2}
+              strokeWidthSecondary={2}
+            />
+          </div>
+        ) : (
+          <div>
+            <div id="stockAnalysisDashboardSubtitle">
+              Put in a stock symbol you'd like to analyze (e.g. MSFT)
+            </div>
+
+            <input
+              value={stockSymbol}
+              onChange={(e) => setStockSymbol(e.target.value)}
+            ></input>
+            <button
+              id="stockAnalysisDashboardButton"
+              onClick={() => runStockAnalysis()}
+            >
+              Analyze
+            </button>
+          </div>
+        )}
       </div>
       {/* <button
           type="button"
